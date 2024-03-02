1d1e4697ec1f45a803f625e6124b42518d66a46f
Carol A. Nikolic <carolnikolic@gmail.com>
2024-03-02 22:21:38 +0100

    wrap try catch of qr generation on if else and console log if url not truth.

--- index.js
+++ index.js
@@ -14,20 +14,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Define the endpoint for generating QR code
 app.post('/generate-qrcode', (req, res) => {
     const url = req.body.url;
-    try {
-        if (url) {
-         // Generate the QR code image dynamically
-    const qr_png = qr.imageSync(url, { type: 'png' });
+    if (url) {
+        try {
+            // Generate the QR code image dynamically
+        const qr_png = qr.imageSync(url, { type: 'png' });
 
-    // Convert the image data to base64 due to Vercel read-only file system
-    const qrBase64 = qr_png.toString('base64');
+        // Convert the image data to base64 due to Vercel read-only file system
+        const qrBase64 = qr_png.toString('base64');
 
-    // Respond with data URL of the QR code image
-    res.send(`data:image/png;base64,${qrBase64}`);
+        // Respond with data URL of the QR code image
+        res.send(`data:image/png;base64,${qrBase64}`);
+        } catch (error) {
+            console.log('Error generating qr code:', error)
+            res.sendStatus(500)
         }
-    } catch (error) {
-        console.log('Error generating qr code:', error)
-        res.sendStatus(500)
+    } else {
+        console.log('Url is false, can not proceed to generate qr code.')
     }
 });
 
